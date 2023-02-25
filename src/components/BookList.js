1acb9cdeb7cb0d6eb415b0b18e98f89bf752fa51
iraklikutchiashvili <irakli9823@gmail.com>
2023-02-26 01:48:44 +0400

    React context. Use theme context to toggle theme

---
+++ src/components/BookList.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+class BookList extends React.Component {
+  // static contextType = ThemeContext;
+
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <div
+              className="book-list"
+              style={{ color: theme.syntax, background: theme.bg }}
+            >
+              <ul>
+                <li style={{ background: theme.ui }}>Book1</li>
+                <li style={{ background: theme.ui }}>Book2</li>
+                <li style={{ background: theme.ui }}>Book3</li>
+              </ul>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
+    );
+  }
+}
+
+export default BookList;
